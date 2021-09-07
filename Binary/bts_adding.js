4663b1115bb0f9924692fc86c5bc3c5e423680a6
noyalreji <84338635+noyalreji@users.noreply.github.com>
2021-09-06 22:12:21 -0500

    Create bts_adding.js

---
+++ Binary/bts_adding.js
@@ -0,0 +1,87 @@
+// Bit Arithmetic
+// Decimal to Binary
+
+// 13
+console.log(0b00001101)
+
+// 6
+console.log(0b00000110)
+
+// 25
+console.log(0b00011001)
+
+// 8
+console.log(0b00001000)
+
+// 45
+console.log(0b0101101)
+
+// 10
+console.log(0b0001010)
+
+// 64
+console.log(0b01000000)
+
+// 255
+console.log(0b11111111)
+
+// 128
+console.log(0b10000000)
+
+// 35
+console.log(0b00100011)
+
+// 0
+console.log(0b00000000)
+
+// 198
+console.log(0b11000110)
+
+// 0b10100101
+// 1+4+32+128
+console.log(1+4+32+128)
+console.log(0b10100101)
+
+// 0b111
+// 1+2+4
+console.log(1+2+4)
+console.log(0b111)
+
+// 0b1111000
+// 8+16+32+64+128
+console.log(8+16+32+64)
+console.log(0b1111000)
+
+// 0b110110
+// 2+4+16+32
+console.log(2+4+16+32)
+console.log(0b110110)
+
+// 0b000
+// 0
+console.log(0b000)
+
+// 0b11
+// 1+2
+console.log(1+2)
+console.log(0b11)
+
+// 0b100110
+// 2+4+32
+console.log(2+4+32)
+console.log(0b100110)
+
+// 0b1010101010
+// 2+8+32+128+512
+console.log(2+8+32+128+512)
+console.log(0b1010101010)
+
+// 0b111001
+// 1+8+16+32
+console.log(1+8+16+32)
+console.log(0b111001)
+
+// 0b100101
+// 1+4+32
+console.log(1+4+32)
+console.log(0b111001)
