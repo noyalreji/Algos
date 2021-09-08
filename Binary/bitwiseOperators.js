df7f3f5bbd729e2e05be3982c522452bb86299d7
Noyal Reji Jacob <84338635+noyalreji@users.noreply.github.com>
2021-09-07 22:28:55 -0500

    Create bitwiseOperators.js

---
+++ Binary/bitwiseOperators.js
@@ -0,0 +1,31 @@
+console.log(0b010101 & 0b0110111);
+console.log(0b010101);
+
+console.log(0b01101001 & 0b00011000);
+console.log(0b00001000);
+
+//0xBABE & //0xBEEF
+console.log(0xBABE & 0xBEEF)
+
+console.log(0b010101 | 0b0110111)
+console.log(0b0110111)
+
+console.log(0b01101001 | 0b00011000)
+console.log(0b01111001)
+
+//0xBABE | 0xBEEF
+
+
+console.log(~0b010101)
+console.log(0b101010)
+
+console.log(~0b0110111)
+console.log(0b1001000)
+
+console.log(~0b01101001);
+console.log(0b10010110);
+
+console.log(~0b0001100)
+console.log(0b1110011)
+
+//~0xBABE ~0xBEEF
